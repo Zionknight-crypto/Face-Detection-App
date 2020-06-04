fa52222ca543263620fcf3eda0c9ad759b8df8e5
Zionknight-crypto <info.davidhauser@gmail.com>
2020-06-04 13:52:41 +0300

    first commit

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './facerecognition.css'
+
+const FacialRecognition = ({imageURL, box}) => {
+    return (
+        <div className='center ma'>
+            <div className='absolute mt2'>
+                <img id='inputimage' src={imageURL} alt='img' width='500px' height='auto' />
+                <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow,left: box.leftCol}}></div>
+            </div>
+        </div>
+    )
+}
+
+export default FacialRecognition;
