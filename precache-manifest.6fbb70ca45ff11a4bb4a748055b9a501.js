ecbff3a6ac1a7e2393dc566bb027547189602f09
Zionknight-crypto <info.davidhauser@gmail.com>
2020-08-07 10:21:39 +0300

    Updates

---
+++ precache-manifest.6fbb70ca45ff11a4bb4a748055b9a501.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed090243a55ae0fad5c9dc1b99e81110",
+    "url": "/Face-Detection-App/index.html"
+  },
+  {
+    "revision": "3397d9ac67e25a590c05",
+    "url": "/Face-Detection-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b0e26f52003b0bdee05e",
+    "url": "/Face-Detection-App/static/css/main.5dd5464d.chunk.css"
+  },
+  {
+    "revision": "3397d9ac67e25a590c05",
+    "url": "/Face-Detection-App/static/js/2.a8a47dce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-Detection-App/static/js/2.a8a47dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e26f52003b0bdee05e",
+    "url": "/Face-Detection-App/static/js/main.e18d2245.chunk.js"
+  },
+  {
+    "revision": "06e8c4fdcfdc21fef773",
+    "url": "/Face-Detection-App/static/js/runtime-main.f0cd5183.js"
+  }
+]);
